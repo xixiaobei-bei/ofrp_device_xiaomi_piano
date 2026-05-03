172f32b6adde3a50da2b5d056dccd21cefff5415
AviderMin <gms2917466108@gmail.com>
2026-05-03 15:04:10 +0800

    feat(恢复): 添加OrangeFox恢复项目支持
    添加fox_piano.mk配置文件并继承OrangeFox特定设置

--- twrp_piano.mk
+++ twrp_piano.mk
@@ -9,6 +9,9 @@ DEVICE_PATH := device/xiaomi/piano
 # Inherit from device.mk configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
 
+# Inherit any OrangeFox-specific settings
+$(call inherit-product-if-exists, $(DEVICE_PATH)/fox_nabu.mk)
+
 ## Device identifier
 PRODUCT_DEVICE := piano
 PRODUCT_NAME := twrp_piano
